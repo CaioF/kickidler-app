8d645da4ee38bdfa60327d53b58d41933a37044f
@CFleury <thecfrt@gmail.com>
2019-09-15 11:17:06 +0300

    Finished form and client side routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React from 'react';
 import './App.css';
-import Form from './components/form'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Form from './components/form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={"logo-big.png"} alt="logo" />
-      </header>
-      <Form></Form>
+    <Router>
+      <div className="App">
+        <ul>
+          <li><Link to="/">HOME</Link></li>
+          <li><Link to="/info">INFO</Link></li>
+        </ul>
+        <header className="App-header">
+          <img src={"logo-big.png"} alt="logo" />
+        </header>
+
+        <hr />
+
+        <Route exact path="/" component={Home} />
+        <Route path="/info" component={Info} />
+      </div>
+    </Router>
+  );
+}
+
+
+function Home() {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
+
+function Info() {
+  return (
+    <div>
+      <h2>Info</h2>
     </div>
   );
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,73 +1,136 @@
 import React, { Component } from 'react'
 
 export default class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      client: "",
+      amount: "",
+      lang: "",
+      seller: "",
+      origin_currency: "",
+      conversion_currency: "",
+      conversion_rate: "",
+      discount_year: "",
+      discount_3years: "",
+      discount_lifetime: ""
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const { target: { name, value } } = event;
+    this.setState({ [name]: value, event: event });
+  }
+
+  handleClick(event) {
+    console.log(this.state);
+    fetch('http://localhost:3000/', {
+      body: JSON.stringify(this.state),
+      cache: 'no-cache',
+      credentials: 'same-origin',
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+      mode: 'cors',
+      redirect: 'follow',
+      referrer: 'no-referrer',
+    })
+      .then(function (response) {
+        console.log(response);
+        if (response.status === 200) {
+          alert('Saved');
+        } else {
+          alert('Issues saving');
+        }
+        // you cannot parse your "success" response, since that is not a valid JSON
+        // consider using valid JSON req/resp pairs.
+        // return response.json();
+      });
+
+  }
+
   render() {
       return (
-        <div className="container-contact100">
-          <div className="wrap-contact100">
-            <form className="contact100-form validate-form">
-              <span className="contact100-form-title">
-                Оформление Коммерческого Предложения
-              </span>
-              <div className="wrap-input100 validate-input bg1">
-                <span className="label-input100">Имя клиента или компании клиента *</span>
-                <input className="input100" type="text" name="name" placeholder="Напишите имена на языке клиента" />
-              </div>
-              <div className="wrap-input100 validate-input bg1 rs1-wrap-input100">
-              <span className="label-input100">Язык КП*</span>
+      <div className="container-contact100">
+        <div className="wrap-contact100">
+          <form name ="myForm" className="contact100-form">
+            <span className="contact100-form-title">
+              Оформление Коммерческого Предложения
+            </span>
+            <div className="wrap-input100">
+              <span className="label-input100">Имя клиента или компании клиента *</span>
+              <input className="input100" type="text" name="client" value={this.state.client} onChange={(event)=>this.handleInputChange(event)} placeholder="Напишите на языке, в котором КП будет оформлено" />
+            </div>
+            <div className="wrap-input100">
+              <span className="label-input100">Количество лицензий *</span>
+              <input className="input100" type="number" name="amount" value={this.state.amount} onChange={(event)=>this.handleInputChange(event)} placeholder="Напишите число" />
+            </div>
+            <div className="wrap-input100 rs1-wrap-input100">
+            <span className="label-input100">Язык КП *</span>
               <div>
-                <select className="js-select2" name="service">
+                <select name="lang" value={this.state.value} onChange={(event)=>this.handleInputChange(event)}>
                   <option>Выберите одно</option>
                   <option>Русский</option>
                   <option>Английский</option>
                   <option>Португальский</option>
                 </select>
               </div>
-              </div>
-              <div className="wrap-input100 bg1 rs1-wrap-input100">
-                  <span className="label-input100">Продавец *</span>
-                  <div>
-                    <select className="js-select2" name="service">
-                      <option>Выберите одно</option>
-                      <option>Кайо</option>
-                      <option>Александер</option>
-                      <option>Владмир</option>
-                      <option>Кирилл</option>
-                    </select>
-                  </div>
-              </div>
-              <div className="wrap-input100 bg1 rs1-wrap-input100">
-                <span className="label-input100">Валюта Перевода *</span>
+            </div>
+            <div className="wrap-input100 rs1-wrap-input100">
+                <span className="label-input100">Продавец *</span>
                 <div>
-                  <input type="radio" name="origin_currency" defaultValue="RUB" defaultChecked />RUB<br />
-                  <input type="radio" name="origin_currency" defaultValue="USD" />USD<br />
+                  <select name="seller" value={this.state.seller} onChange={(event)=>this.handleInputChange(event)}>
+                    <option>Выберите одно</option>
+                    <option>Кайо</option>
+                    <option>Александер</option>
+                    <option>Владмир</option>
+                    <option>Кирилл</option>
+                  </select>
                 </div>
-                <span className="label-input100">Валюта Клиента *</span>
-                <div>
-                <select className="js-select2" name="service">
+            </div>
+            <div className="wrap-input100 rs1-wrap-input100">
+              <span className="label-input100">Валюта Перевода *</span>
+              <div>
+                  <select name="origin_currency" value={this.state.origin_currency} onChange={(event)=>this.handleInputChange(event)}>
                   <option>Выберите одно</option>
                   <option>RUB</option>
                   <option>USD</option>
+                </select>
+              </div>
+              <span className="label-input100">Валюта Клиента *</span>
+              <div>
+                <select name="conversion_currency" value={this.state.conversion_currency} onChange={(event)=>this.handleInputChange(event)}>
+                  <option>Выберите одно</option>
+                  <option>Без конверции валют</option>
                   <option>EUR</option>
                   <option>BRL</option>
                 </select>
-                </div>
               </div>
-              <div className="wrap-input100 bg1 rs1-wrap-input100">
-                <span className="label-input100">Курс валют</span>
-                <textarea className="input100" name="service" placeholder="Если RUB к RUB или USD к USD напишите 1" defaultValue={""} />
-              </div>
-              <div className="container-contact100-form-btn">
-                <button className="contact100-form-btn">
-                  <span>
-                    Оформить
-                    <i className="fa fa-long-arrow-right m-l-7" aria-hidden="true" />
-                  </span>
-                </button>
-              </div>
-            </form>
-          </div>
+            </div>
+            <div className="wrap-input100 rs1-wrap-input100">
+              <span className="label-input100">Курс валют</span>
+              <textarea className="input100" name="conversion_rate" value={this.state.conversion_rate} onChange={(event)=>this.handleInputChange(event)} placeholder="Если валюта клиента = RUB или USD — оставьте это поле пусто и выберите 'Без конверции валют' в поле 'Валюта Клиента'"/>
+            </div>
+            <div className="wrap-input100">
+              <span className="label-input100">Скидки *</span>
+              <input className="input100" type="number" name="discount_year" value={this.state.discount_year} onChange={(event)=>this.handleInputChange(event)} placeholder="Скидка на годовых — 0 = без скидки, 100 = бесплатно" />
+              <input className="input100" type="number" name="discount_3years" value={this.state.discount_3years} onChange={(event)=>this.handleInputChange(event)} placeholder="Скидка на 3 годовых — 0 = без скидки, 100 = бесплатно" />
+              <input className="input100" type="number" name="discount_lifetime" value={this.state.discount_lifetime} onChange={(event)=>this.handleInputChange(event)} placeholder="Скидка на бессрочных — 0 = без скидки, 100 = бесплатно" />
+            </div>
+            <div className="container-contact100-form-btn">
+              <button type="button" className="contact100-form-btn" onClick={(event)=>this.handleClick(event)}>
+                <span>
+                  Оформить
+                </span>
+              </button>
+            </div>
+          </form>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
