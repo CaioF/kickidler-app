ee63728aa33b5ae935dc15f6832b6b062a2163cb
k.fleury <k.fleury@kickidler.com>
2019-09-16 17:57:54 +0300

    Added initial functionality to serving dynamic PDF

--- server-build.js
+++ server-build.js
@@ -3,14 +3,38 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const pdfMakePrinter = require('pdfmake/src/printer');
 const app = express();
-
-var docDefinition = {
-  content: ['тест']
+//json from form//
+global.JSONanswer = {};
+//pdfmake//
+global.PDFlanguages = {
+  rus: {
+    one: "это",
+    two: "тест"
+  },
+  eng: {
+    one: "this is",
+    two: "a test"
+  },
+  por: {
+    one: "isso é",
+    two: "um teste"
+  },
 };
+global.PDFlang = {};
+global.DOCdefinition = {};
 
 function generatePdf(docDefinition, callback) {
+  console.log(JSONanswer);
   try {
-    const fontDescriptors = { ... };
+    const fontDescriptors = {
+      Roboto: {
+    		normal: 'build/fonts/Roboto-Regular.ttf',
+    		bold: 'build/fonts/Roboto-Medium.ttf',
+    		italics: 'build/fonts/Roboto-Italic.ttf',
+    		bolditalics: 'build/fonts/Roboto-MediumItalic.ttf'
+	    }
+    };
+
     const printer = new pdfMakePrinter(fontDescriptors);
     const doc = printer.createPdfKitDocument(docDefinition);
 
@@ -30,25 +54,77 @@ function generatePdf(docDefinition, callback) {
     throw(err);
   }
 };
+///////////////////////////////////////////////////////////
 
+//server//
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());
 
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.get('/send', function(req, res) {
-  generatePdf(docDefinition, (response) => {
+
+  switch (JSON.stringify(JSONanswer.lang)) {
+  case '"Русский"':
+    PDFlang = PDFlanguages.rus;
+    break;
+  case '"Английский"':
+    PDFlang = PDFlanguages.eng;
+    break;
+  case '"Португальский"':
+    PDFlang = PDFlanguages.por;
+    break;
+  default:
+    break;
+  };
+
+  DOCdefinition = {
+    content: [
+      { text: 'Tables', style: 'header' },
+      { text: JSON.stringify(PDFlang.one) },
+      { text: JSON.stringify(PDFlang.two), style: 'subheader' },
+      ':)',
+      {
+        style: 'tableExample',
+        table: {
+          body: [
+            ['Column 1', 'Column 2', 'Column 3'],
+            [{ text: JSON.stringify(JSONanswer.client) }, { text: JSON.stringify(JSONanswer.amount) }, 'OK?']
+          ]
+        }
+      },
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10]
+      },
+      subheader: {
+        fontSize: 16,
+        bold: true,
+        margin: [0, 10, 0, 5]
+      },
+      tableExample: {
+        margin: [0, 5, 0, 15]
+      },
+    },
+    defaultStyle: {
+      //alignment: 'justify'
+    }
+  };
+  generatePdf(DOCdefinition, (response) => {
     res.setHeader('Content-Type', 'application/pdf');
     res.send(response); // Buffer data
   });
 });
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.post('/send', function (req, res) {
-  console.log(req.body);
+  JSONanswer = req.body;
   res.redirect('../send');
 });
 
-app.listen(3000);
-console.log("Server is running on port 3000");
+app.listen(8008);
+console.log("Server is running on port 8008, set to local IP 192.168.19.217, to change the IP edit form.jsx file and rebuild");
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -27,7 +27,7 @@ export default class Form extends Component {
 
   handleClick(event) {
     console.log(this.state);
-    fetch('http://localhost:3000/send', {
+    fetch('http://192.168.19.217:8008/send', {
       body: JSON.stringify(this.state),
       cache: 'no-cache',
       credentials: 'same-origin',
@@ -42,7 +42,7 @@ export default class Form extends Component {
       .then(function (response) {
         console.log(response);
         if (response.status === 200) {
-          alert('Ваше КП готово!');
+          setTimeout(function(){  window.location='http://192.168.19.217:8008/send' }, 700);
         } else {
           alert('Ошибка сервера!');
         }
